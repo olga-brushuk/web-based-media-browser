897e8c0b3d27133c2cf1a6748f7b94b298a6146d
Olga Brushuk <olga@konstructive.com>
2020-07-27 10:52:48 +0100

    Updates

---
+++ precache-manifest.ece972b36647981d42f19cd6c2b017cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac29c21b954a43db674f1717ee161afc",
+    "url": "/web-based-media-browser/index.html"
+  },
+  {
+    "revision": "f2049c3db3e8ab5da9b2",
+    "url": "/web-based-media-browser/static/css/main.e6e3c902.chunk.css"
+  },
+  {
+    "revision": "bb33a101a9e869123ad0",
+    "url": "/web-based-media-browser/static/js/2.f6e63920.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/web-based-media-browser/static/js/2.f6e63920.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2049c3db3e8ab5da9b2",
+    "url": "/web-based-media-browser/static/js/main.eff0e9b6.chunk.js"
+  },
+  {
+    "revision": "b4096a9a488eccc9561f",
+    "url": "/web-based-media-browser/static/js/runtime-main.f45b1a6f.js"
+  }
+]);
